dabf23cc544a6fbf6dcd571b7a3bd00339127500
Koray Tug <koray.tug@virtudev.net>
2021-03-24 22:52:56 +0300

    add homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <spann className='subtitle'>SHOP NOW</spann>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JAKETS</h1>
+                    <spann className='subtitle'>SHOP NOW</spann>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <spann className='subtitle'>SHOP NOW</spann>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS</h1>
+                    <spann className='subtitle'>SHOP NOW</spann>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>MENS</h1>
+                    <spann className='subtitle'>SHOP NOW</spann>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
