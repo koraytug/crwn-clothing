a47798e7826b6df948e81925822a739b835b98de
Koray Tug <koray.tug@virtudev.net>
2021-04-10 18:14:38 +0300

    add hadder component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-      <div>
-          <Switch>
-            <Route exact path='/' component={HomePage}></Route>
-            <Route path='/shop' component={ShopPage}></Route>
-          </Switch>
-
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
