35f9f9baccaf213129d0f8a7cbc32b8d72f2f1e6
Koray Tug <koray.tug@virtudev.net>
2021-06-06 21:03:26 +0300

    add collections to firebase

--- src/App.js
+++ src/App.js
@@ -16,11 +16,13 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+// import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    //   const { setCurrentUser, collectionsArray } = this.props;
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
@@ -36,6 +38,10 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      //   addCollectionAndDocuments(
+      //     'collections',
+      //     collectionsArray.map(({ title, items }) => ({ title, items }))
+      //   );
     });
   }
 
@@ -70,6 +76,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
+  //   collectionsArray: selectCollectionsForPreview,
 });
 
 const mapDispatchToProps = (dispatch) => ({
